744368abd3cde863975bac18c18dd29489e1c3da
Marc Girbau <marc.girbau94@gmail.com>
2023-07-09 00:17:52 +0200

    Add theme options for switch mui component

--- src/styles/colors.jsx
+++ src/styles/colors.jsx
@@ -1,6 +1,9 @@
 export const backgroundLight = "#fff";
 export const backgroundDark = "#09090B";
 
+export const lightColor = "#fff";
+export const darkColor = "#09090B";
+
 export const backgroundButtonLight = "#f4f4f5";
 export const backgraoundButtonDark = "#27272a";
 
