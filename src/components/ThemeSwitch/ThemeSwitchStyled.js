2201044eae11d9a039c1f3710e3f8420f4f5aaee
Marc Girbau <marc.girbau94@gmail.com>
2023-07-10 00:41:00 +0200

    Include icon to ThemeSwitch component

---
+++ src/components/ThemeSwitch/ThemeSwitchStyled.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const ThemeSwitchStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2px;
+`;
+
+export default ThemeSwitchStyled;
