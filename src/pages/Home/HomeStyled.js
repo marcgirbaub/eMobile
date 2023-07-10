3e104c95503717d029ec828298a937333a64926f
Marc Girbau <marc.girbau94@gmail.com>
2023-07-10 14:12:21 +0200

    Style Home top section with search bar

--- src/pages/Home/HomeStyled.js
+++ src/pages/Home/HomeStyled.js
@@ -1,7 +1,10 @@
 import styled from "@emotion/styled";
 
 const HomeStyled = styled.div`
-  padding: 20px;
+  padding: 40px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
 
   .mobiles {
     display: grid;
@@ -10,6 +13,31 @@ const HomeStyled = styled.div`
     justify-content: center;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     grid-column-gap: 20px;
+  }
+
+  .top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 1000px) {
+      flex-direction: column;
+      gap: 20px;
+      text-align: center;
+      line-height: 38px;
+    }
+
+    &__slogan {
+      margin-right: 14px;
+    }
+
+    &__search {
+      @media (min-width: 600px) {
+        max-width: 350px;
+      }
+
+      width: 100%;
+    }
   }
 `;
 
