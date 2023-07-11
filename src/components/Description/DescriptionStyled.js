7bd951853ff6145972a0fdc70067d619cb92378f
Marc Girbau <marc.girbau94@gmail.com>
2023-07-11 18:12:06 +0200

    Create Description component and its styles

---
+++ src/components/Description/DescriptionStyled.js
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+const DescriptionStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  .description {
+    &__list {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+    }
+
+    &__price {
+      font-weight: bold;
+      font-size: 1.6rem;
+    }
+
+    &__title {
+      font-weight: bold;
+    }
+
+    &__value {
+      font-weight: normal;
+    }
+  }
+
+  .horizontal-separator {
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.palette.secondary.light};
+    margin: 0 auto;
+  }
+`;
+
+export default DescriptionStyled;
