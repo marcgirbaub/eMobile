6dd50f57a64ec0446b641407f34c7560098e4eb7
Marc Girbau <marc.girbau94@gmail.com>
2023-07-11 18:35:26 +0200

    Implement storage and color selection in Actions component

--- src/components/Actions/ActionsStyled.js
+++ src/components/Actions/ActionsStyled.js
@@ -13,6 +13,15 @@ const ActionsStyled = styled.div`
       padding: 6px 12px;
       border-radius: 6px;
       font-size: 0.9rem;
+
+      &:hover {
+        opacity: 0.8;
+      }
+
+      &--selected {
+        background-color: ${({ theme }) => theme.palette.contrast};
+        color: ${({ theme }) => theme.palette.background.default};
+      }
     }
 
     &__buttons-container {
@@ -33,6 +42,14 @@ const ActionsStyled = styled.div`
       font-size: 0.9rem;
       color: ${({ theme }) => theme.palette.background.default};
       margin-bottom: 6px;
+
+      &:disabled {
+        opacity: 0.7;
+      }
+
+      &:hover {
+        opacity: 0.9;
+      }
     }
   }
 `;
