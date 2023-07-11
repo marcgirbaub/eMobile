1a204d5805aa9c40af2f3d8442d6456647fada86
Marc Girbau <marc.girbau94@gmail.com>
2023-07-11 18:44:03 +0200

    Include Actions and Description components in DetailPage

--- src/pages/DetailPage/DetailPageStyled.js
+++ src/pages/DetailPage/DetailPageStyled.js
@@ -10,74 +10,18 @@ const DetailPageStyled = styled.div`
     &__image {
       border-radius: 4px;
     }
+
+    &__info {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+    }
   }
 
   @media (max-width: 864px) {
     flex-direction: column;
     align-items: center;
     gap: 40px;
-  }
-
-  .info {
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
-
-    &__list {
-      display: flex;
-      flex-direction: column;
-      gap: 8px;
-    }
-
-    &__price {
-      font-weight: bold;
-      font-size: 1.6rem;
-    }
-
-    &__title {
-      font-weight: bold;
-    }
-
-    &__value {
-      font-weight: normal;
-    }
-
-    &__actions {
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-    }
-  }
-
-  .actions {
-    &__button {
-      background-color: ${({ theme }) => theme.palette.backgroundButton};
-      border: 1px solid ${({ theme }) => theme.palette.secondary.light};
-      font-weight: bold;
-      padding: 6px 12px;
-      border-radius: 6px;
-      font-size: 0.9rem;
-    }
-
-    &__buttons-container {
-      display: flex;
-      gap: 14px;
-    }
-
-    &__title {
-      margin-bottom: 10px;
-    }
-
-    &__add-button {
-      background-color: ${({ theme }) => theme.palette.contrast};
-      border: 1px solid ${({ theme }) => theme.palette.secondary.light};
-      font-weight: bold;
-      padding: 10px 12px;
-      border-radius: 6px;
-      font-size: 0.9rem;
-      color: ${({ theme }) => theme.palette.background.default};
-      margin-bottom: 6px;
-    }
   }
 
   .horizontal-separator {
