a652c041bd062731e97583a394144034f6ff7923
Marc Girbau <marc.girbau94@gmail.com>
2023-07-13 13:11:51 +0200

    Install framer motion and define variants

---
+++ src/styles/framerMotionVariants.js
@@ -0,0 +1,27 @@
+export const downVariants = {
+  initial: {
+    opacity: 0,
+    y: 20,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+export const leftVariants = {
+  initial: {
+    opacity: 0,
+    x: -20,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
